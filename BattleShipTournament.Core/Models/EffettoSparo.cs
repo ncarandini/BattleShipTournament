5ac66b744dcd36fa32152734e19ce236f10d009e
ncarandini <n.carandini@outlook.com>
2018-04-10 16:13:37 +0200

    Add Core library

---
+++ BattleShipTournament.Core/Models/EffettoSparo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipTournament.Core.Models
+{
+    public enum EffettoSparo
+    {
+        Acqua,
+        Colpito,
+        Affondato
+    }
+}
---
+++ BattleShipTournament.Core/Models/Interfaces/IAdmiral.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipTournament.Core.Models
+{
+    public interface IAdmiral
+    {
+        string Nome { get; }
+
+        void PosizionaFlotta();
+
+        Coordinate Spara();
+
+        EffettoSparo Rapporto();
+
+        event Action<IAdmiral> FlottaAffondata;
+    }
+}
