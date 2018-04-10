2b91ed0c63d5d59fc94e7b53efc7e3dd3cefa8af
ncarandini <n.carandini@outlook.com>
2018-04-10 16:13:50 +0200

    Add Nick library

---
+++ BattleShipTournament.Nick/AdmiralNick.cs
@@ -0,0 +1,37 @@
+﻿using BattleshipTournament.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Nick
+{
+    public class AdmiralNick : IAdmiral
+    {
+        public event Action<IAdmiral> FlottaAffondata;
+
+        readonly string nome;
+        public string Nome { get { return nome; } }
+
+        public AdmiralNick(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void PosizionaFlotta()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EffettoSparo Rapporto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Coordinate Spara()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
