8681e826d880e576f4c5ac0e7e0e023bc6e0402e
ncarandini <n.carandini@outlook.com>
2018-04-10 15:40:10 +0200

    Add solution Add first things

---
+++ BattleshipTournament.ConsoleApp/Models/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipTournament.ConsoleApp.Models
+{
+    public struct Coordinate
+    {
+        public readonly int Riga;
+        public readonly int Colonna;
+
+        public Coordinate(int riga, int colonna)
+        {
+            Riga = riga;
+            Colonna = colonna;
+        }
+    }
+}
