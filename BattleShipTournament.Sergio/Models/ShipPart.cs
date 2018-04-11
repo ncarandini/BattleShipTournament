2c90796b8156de9e06e2fc7561a78d37d7fd2a49
Sergioceliento <celse@hotmail.it>
2018-04-11 11:14:02 +0200

    Add classes Mappa, Posizionamento, ShipPart, Sparo, StatusShipPart - AdmiralSergio

---
+++ BattleShipTournament.Sergio/Models/ShipPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Sergio.Models
+{
+    internal struct SeaPart
+    {
+        public Ship Ship;
+        public int internalindex;
+    }
+}
