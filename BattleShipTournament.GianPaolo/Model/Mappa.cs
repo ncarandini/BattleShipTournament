7eac396b34950f90be681f3bbbc19bc3fb82f586
gianpaolorusso <gian962001@live.it>
2018-04-11 18:02:41 +0200

    repaire mapclass

--- BattleShipTournament.GianPaolo/Model/Mappa.cs
+++ BattleShipTournament.GianPaolo/Model/Mappa.cs
@@ -8,15 +8,21 @@ using System.Threading.Tasks;
 namespace BattleShipTournament.GianPaolo.Model
 {
 
-    public class Mappa
+    public class Mappa:IMappa
     {
         private static Mappa instance;
-        private  List<Nave> ListaNavi { get;set; }
-
-        public Mappa() { }
+        List<Nave>  ListaNavi = new List<Nave>()
+        {
+        new Nave("sottomarino",1),
+        new Nave("corvetta",2),
+        new Nave("fregata",3),
+        new Nave("cacciatorpediniere",4),
+        new Nave("portaerei",5)
+        };
 
         public static Mappa Instanza()
         {
+           
             if(instance==null)
             {
                 instance = new Mappa();
@@ -24,6 +30,42 @@ namespace BattleShipTournament.GianPaolo.Model
             return instance;
         }
 
-        public 
+        public void Posiziona()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RiceviColpo(int x, int y)
+        {
+            return CoordinateColpoRicevuto(x, y);
+        }
+
+        public String CoordinateColpoRicevuto(int x,int y)
+        { String message = "acqua";
+            bool trovato = false;
+            foreach(Nave n in ListaNavi)
+            {
+                for(int j=0; j<n.PartiNave.Count; j++)
+                {
+                    if(n.PartiNave[j].X==x && n.PartiNave[j].Y==y)
+                    {
+                        n.PartiNave[j].SetStato("colpita");
+                        message = "colpita";                      
+                        trovato = true;
+                        break;
+                    }
+                    if (n.Affondata())
+                    { message = "colpita e affondata"; }
+                }
+                if(trovato)
+                { break; }
+            }
+            return message;
+        }
+
+        public string Sparo(int x, int y)
+        {
+            return "";
+        }
     }
 }
--- BattleShipTournament.GianPaolo/Model/Nave.cs
+++ BattleShipTournament.GianPaolo/Model/Nave.cs
@@ -10,34 +10,55 @@ namespace BattleShipTournament.GianPaolo.Models
 {
     class Nave
     {
-        public int Lunghezza { get; set; }
-        public string Nome { get; set; }
-        public  List<ParteNave> PartiNave;
+        int Lunghezza { get; set; }
+        string Nome { get; set; }
+        public List<ParteNave> PartiNave { get;  set; }
         Posizione posizione = new Posizione();
-
-       
-
-        
-
+        //usata nel metodo RRiceviCoordinateSparo in Mappa
+        public bool NaveAffondata = false;
+        Guid guid;
         public Nave(string nome,int lunghezza)
         {
+           
             this.Lunghezza = lunghezza;
             this.Nome = nome;
             PartiNave = new List<ParteNave>();
             for(int i=0; i<lunghezza;  i++ )
             {
+
+                guid = new Guid();
                 ParteNave Parte = new ParteNave
                 {                 
-                    Stato = "Intatto"
+                    Stato = "Intatta"
                 };
                 PartiNave.Add(Parte);
             }         
         }
         public void posizionaNave(int x,int y)
-
         {
             posizione.X = x;
             posizione.Y = y;
         }
+
+        public int  GetLunghezza()
+        { return Lunghezza; }
+
+        //usato nella mappa nel metodo RiceviCoordinateSparo Mappa
+        public bool Affondata()
+        {
+            int numpart = 0;
+          
+            foreach(ParteNave parte in PartiNave)
+            {
+                if(parte.Stato=="colpita")
+                { ++numpart; }
+            }
+            if (numpart == Lunghezza)
+            {
+                this.NaveAffondata = true;
+            }
+            return this.NaveAffondata;
+        }
+       
     }
 }
