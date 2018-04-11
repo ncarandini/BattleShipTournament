449fac5597be3670756a689cc16dd3ea7c7ec8e6
NikoStrappelli <tacos.93@hotmail.it>
2018-04-11 18:07:20 +0200

    AdmiralNiko v.0.1 completato

---
+++ BattleShipTournament.Niko/Models/Nave.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Niko.Models
+
+    // è composta da piu parti di nave in base alla propria lunghezza
+
+{
+    public class Nave
+    {
+        int lunghezza;
+
+        StatusParteNave[] statusNave;
+
+        public Nave(int l)
+        {
+            lunghezza = l;
+            statusNave = new StatusParteNave[l];
+
+            for (int i = 0; i < l; i++)
+            {
+                statusNave[i] = StatusParteNave.Good;
+            }
+        }
+
+        public bool Colpita (int partIndex)
+        {
+            if ( partIndex < 0 || partIndex >= lunghezza)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            statusNave[partIndex] = StatusParteNave.Damaged;
+
+            bool affondata = true;
+            foreach (var statusParteNave in statusNave)
+            {
+                if (statusParteNave == StatusParteNave.Good)
+                {
+                    affondata = false;
+                    break;
+                }
+            }
+            return affondata;
+        }
+    }
+    
+}
