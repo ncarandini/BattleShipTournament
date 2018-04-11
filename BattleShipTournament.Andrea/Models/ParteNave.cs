8c1c41f6463033d8448887aa1d08be8fa927c6c9
burgioandrea <andreaburgio1993@gmail.com>
2018-04-11 12:33:02 +0200

    Crerated ParteNave Class

---
+++ BattleShipTournament.Andrea/Models/ParteNave.cs
@@ -0,0 +1,24 @@
+﻿using BattleshipTournament.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Andrea.Models
+{
+    struct ParteNave
+    {
+        public Coordinate Coordinate { get; set; }
+        public StatoParteNave StatoParteNave { get; set; }
+
+        public ParteNave(Coordinate coordinate, StatoParteNave statoParteNave)
+        {
+            Coordinate = coordinate;
+            StatoParteNave = statoParteNave;
+        }
+
+
+
+    }
+}
