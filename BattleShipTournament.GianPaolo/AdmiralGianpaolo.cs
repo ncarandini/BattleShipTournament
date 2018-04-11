9c9af5958c61d31fbc11488d51cd28ef8e9a4c4b
gianpaolorusso <gian962001@live.it>
2018-04-11 18:01:32 +0200

    add Interface in my models

--- BattleShipTournament.GianPaolo/AdmiralGianpaolo.cs
+++ BattleShipTournament.GianPaolo/AdmiralGianpaolo.cs
@@ -1,5 +1,5 @@
 ﻿using BattleshipTournament.Core.Models;
-using BattleShipTournament.GianPaolo.Models;
+using BattleShipTournament.GianPaolo.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,32 +10,28 @@ namespace BattleShipTournament.GianPaolo
 {
     public class AdmiralGianpaolo : IAdmiral
     {
-        List<Nave> flotta = new List<Nave>()
-        {
-        new Nave("sottomarino",1),
-        new Nave("corvetta",2),
-        new Nave("fregata",3),
-        new Nave("cacciatorpediniere",4),
-        new Nave("portaerei",5)
-        };
+
+        Mappa mappa = Mappa.Instanza();
 
         public string Nome => "Gianpaolo";
 
         public event Action<IAdmiral> FlottaAffondata;
+
         public AdmiralGianpaolo()
         { }
 
-        public void PosizionaFlotta()
-        {
-            throw new NotImplementedException();
-        }
 
         public EffettoSparo Rapporto()
         {
-            throw new NotImplementedException();
+            
         }
 
         public Coordinate Spara()
+        {
+            
+        }
+
+        public void PosizionaFlotta()
         {
             throw new NotImplementedException();
         }
