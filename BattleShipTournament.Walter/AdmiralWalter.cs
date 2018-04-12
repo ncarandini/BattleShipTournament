42086e5b4441467d0c57ee01abb1f005e39e3188
Walter <walter.mongelluzzo@gmail.com>
2018-04-12 18:13:11 +0200

    implementato Artigliere

--- BattleShipTournament.Walter/AdmiralWalter.cs
+++ BattleShipTournament.Walter/AdmiralWalter.cs
@@ -16,7 +16,8 @@ namespace BattleShipTournament.Walter
 
         private List<Nave> laMiaFlotta;
         private List<Coordinate> posizioniOccupate; //le posizioni occupate dalla mia flotta
-        private List<Coordinate> campoNemico;       //le posizioni in cui ho sparato
+        //private List<Coordinate> campoNemico;       //le posizioni in cui ho sparato
+        private Artigliere artigliere;
         
 
         public AdmiralWalter()
@@ -37,7 +38,8 @@ namespace BattleShipTournament.Walter
             posizioniOccupate= new List<Coordinate>();
 
             //creo il campo di battaglia nemico
-            campoNemico = new List<Coordinate>();
+            //campoNemico = new List<Coordinate>();
+            artigliere = new Artigliere();
            
         }
 
@@ -53,7 +55,7 @@ namespace BattleShipTournament.Walter
 
                 //genero random la posizione iniziale della nave
                 Nave naveDaPosizionare = laMiaFlotta[indice];
-                Coordinate posizioneIniziale = generaCoordinataRandom();
+                Coordinate posizioneIniziale = GeneratoreCoordinateRandom.generaCoordinata();
                 //provo a posizionare la nave
                 bool flag = true;
                 List<Coordinate> prova = naveDaPosizionare.impostaPosizione(posizioneIniziale, versoNave);
@@ -99,37 +101,16 @@ namespace BattleShipTournament.Walter
             return effettoSparo;
         }
 
+
         public Coordinate Spara()
         {
-            Coordinate sparo;
-
-            while(true)
-            {               
-                sparo = generaCoordinataRandom();
-                //se non ho già provato a sparare in questa posizione
-                if(!campoNemico.Contains(sparo))
-                {
-                    campoNemico.Add(sparo);
-                    break;
-                }
-            }
-
-            return sparo;
+          return artigliere.spara();
         }
 
-        private Coordinate generaCoordinataRandom()
-        {
-            Random random = new Random();
-            int riga = random.Next(0, 10);
-            int colonna = random.Next(0, 10);
-            Coordinate coordinata = new Coordinate(riga, colonna);
-            return coordinata;
-
-        }
 
         public void RiceviRapporto(EffettoSparo effettoSparo)
         {
-            throw new NotImplementedException();
+            artigliere.verificaUltimoSparo(effettoSparo);
         }
     }
 }
---
+++ BattleShipTournament.Walter/Models/GeneratoreCoordinateRandom.cs
@@ -0,0 +1,20 @@
+﻿using BattleshipTournament.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Walter.Models
+{
+    internal static class GeneratoreCoordinateRandom
+    {
+        public static Coordinate generaCoordinata()
+        {
+            Random rand = new Random();
+            int i = rand.Next(0, 10);
+            int j = rand.Next(0, 10);
+            return new Coordinate(i, j);
+        }
+    }
+}
