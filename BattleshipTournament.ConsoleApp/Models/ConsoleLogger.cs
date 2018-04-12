0492e3754400206a98a860c2137e723860d66bf7
ncarandini <n.carandini@outlook.com>
2018-04-12 09:52:01 +0200

    Version 0.9 of Game managing done!

---
+++ BattleshipTournament.ConsoleApp/Models/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipTournament.ConsoleApp.Models
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(object sender, string message)
+        {
+            Console.WriteLine($"{DateTime.Now} [{sender.GetType().Name}] {message}");
+        }
+    }
+}
