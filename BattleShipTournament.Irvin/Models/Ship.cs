c4006ca43fbd682b6600ae98a8aa2249f9f5b626
Administrator <Administrator@WIN-359GB4B0Q4E>
2018-04-11 18:06:59 -0700

    AdmiralIrvin Versione 0.1 completato

---
+++ BattleShipTournament.Irvin/Models/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipTournament.Irvin.Models
+{
+    public class Ship
+    {
+        private int lunghezza;
+
+        private StatusParteNave[] statusNave;
+
+        public Ship(int lunghezza)
+        {
+            this.lunghezza = lunghezza;
+            statusNave = new StatusParteNave[lunghezza];
+
+            for(int i = 0; i < lunghezza; i++)
+            {
+                statusNave[i] = StatusParteNave.Good;
+            }
+        }
+
+        public bool Colpita(int partIndex)
+        {
+            if (partIndex < 0 || partIndex >= lunghezza)
+            {
+                throw new ArgumentOutOfRangeException();    // se ricevo un risultato minore o maggiore della misura di una qualsiasi nave, genero errore
+            }
+
+            statusNave[partIndex] = StatusParteNave.Damaged;
+
+            bool affondata = true;
+
+            foreach (var p in statusNave)   //cicla per ogni p in statusNave
+            {
+                if (p == StatusParteNave.Good)  //se p risulta almeno una parte della nave NON colpita
+                {
+                    affondata = false;  // la nave non è affondata
+                    break;  // interrompo ciclo
+                }
+            }
+
+            return affondata;   // ritorna che la nave è affondata
+        }
+    }
+}
