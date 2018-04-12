c6509ba93a43ceb1d82c64bd85c7b6b8936c5e8e
Daniele <daniele_fuscaldo@yahoo.com>
2018-04-11 18:06:07 -0700

    Aggiunta classi

--- BattleShipTournament.Daniele/AdmiralDaniele.cs
+++ BattleShipTournament.Daniele/AdmiralDaniele.cs
@@ -1,4 +1,5 @@
 ﻿using BattleshipTournament.Core.Models;
+using BattleShipTournament.Daniele.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,21 +12,63 @@ namespace BattleShipTournament.Daniele
     {
         public string Nome => "Daniele";
 
+        Stratega stratega;
+        Tattico tattico;
+
+
         public event Action<IAdmiral> FlottaAffondata;
+
+        Nave[] flotta;
+
+
+        //costruttore ammiraglio
+        public AdmiralDaniele()
+        {
+            stratega = new Stratega();
+            tattico = new Tattico();
+
+            flotta = new Nave[5]
+            {
+                new Nave(1),
+                new Nave(2),
+                new Nave(3),
+                new Nave(4),
+                new Nave(5)
+            };
+
+        }
 
         public void PosizionaFlotta()
         {
-            throw new NotImplementedException();
+            stratega.PosiozionaNavi(flotta);
         }
 
-        public EffettoSparo Rapporto()
+        public EffettoSparo Rapporto(Coordinate sparo)
         {
-            throw new NotImplementedException();
+            EffettoSparo effetto = stratega.RiceviColpoEFaiRapporto(sparo);
+
+            if (FleetIsDestroyed())
+            {
+                FlottaAffondata?.Invoke(this);
+
+            }
+            return stratega.RiceviColpoEFaiRapporto(sparo);
+        }
+
+        private bool FleetIsDestroyed()
+        {
+            //TODO
+            return false;
         }
 
         public Coordinate Spara()
         {
-            throw new NotImplementedException();
+            return tattico.Spara();
+        }
+
+        public void RiceviRapporto(EffettoSparo effettoSparo)
+        {
+            tattico.RisultatoSparo(effettoSparo);
         }
     }
 }
